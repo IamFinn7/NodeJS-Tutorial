144bbb3f7953f448c0c43460f718d53bb80ef8f3
Finn <cpn2003.py@gmail.com>
2024-07-27 17:08:43 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const path = require('path');
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const configViewEngine = require("./config/viewEngine");
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 3333;
 const hostname = process.env.HOST_NAME;
 
 //config template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+configViewEngine(app);
 
 //khai báo route
-app.get('/', (req, res) => {
-  res.send('Hello World! and nodemon')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World! and nodemon");
+});
 
-app.get('/finn', (req, res) => {
-    // res.send('Check check!')
-    res.render('sample.ejs')
-  })
+app.get("/finn", (req, res) => {
+  // res.send('Check check!')
+  res.render("sample.ejs");
+});
 
 app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
